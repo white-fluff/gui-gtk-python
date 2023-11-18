e487b92311ccd7d680eec3ab94809ffc4eea12ec
Konstantin <konstantin.belopuhov@gmail.com>
2023-11-18 15:44:30 +0600

    add file-chooser open dialog

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Repo with code about learning how to create GUI app with Python and GTK.
 
 **Links:**
- - [Tutorial](https://github.com/Taiko2k/GTK4PythonTutorial#adding-a-slider-aka-scale)
+ - [Tutorial](https://github.com/Taiko2k/GTK4PythonTutorial)
  - [GTK4 Doc](https://docs.gtk.org/gtk4/)
  - [GTK4 Widget Gallery](https://docs.gtk.org/gtk4/visual_index.html)
  - [Adwaita Widget Gallery](https://gnome.pages.gitlab.gnome.org/libadwaita/doc/main/widget-gallery.html)
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 
-from gi.repository import Gtk, Adw, Gdk
+from gi.repository import Gtk, Adw, Gdk, GLib, Gio
 
 
 css_provider = Gtk.CssProvider()
@@ -54,6 +54,31 @@ class MainWindow(Gtk.ApplicationWindow):
         self.slider.set_value(5)
         self.slider.connect('value-changed', self.slider_changed)
         self.box2.append(self.slider)
+
+        # HeadBar
+        self.header = Gtk.HeaderBar()
+        self.set_titlebar(self.header)
+
+        # Open dialog (File chooser)
+        self.open_button = Gtk.Button(label="Open")
+        self.open_button.set_icon_name("document-open-symbolic")
+        self.header.pack_start(self.open_button)
+        self.open_button.connect('clicked',  self.show_open_dialog)
+
+        self.open_dialog = Gtk.FileDialog.new()
+        self.open_dialog.set_title("Select a File")
+
+        # File chooser filter
+        f = Gtk.FileFilter()
+        f.set_name("Image files")
+        f.add_mime_type("image/jpeg")
+        f.add_mime_type("image/png")
+
+        filters = Gio.ListStore.new(Gtk.FileFilter)  # Create a ListStore with the type Gtk.FileFilter
+        filters.append(f)  # Add the file filter to the ListStore. You could add more.
+
+        self.open_dialog.set_filters(filters)  # Set the filters for the open dialog
+        self.open_dialog.set_default_filter(f)
         
         # Boxes
         self.set_child(self.box1)
@@ -75,6 +100,19 @@ class MainWindow(Gtk.ApplicationWindow):
     
     def slider_changed(self, slider):
         print(int(slider.get_value()))
+
+
+    def show_open_dialog(self, button):
+        self.open_dialog.open(self, None, self.open_dialog_open_callback)
+
+    def open_dialog_open_callback(self, dialog, result):
+        try:
+            file = dialog.open_finish(result)
+            if file is not None:
+                print(f"File path is {file.get_path()}")
+                # Handle loading file from here
+        except GLib.Error as error:
+            print(f"Error opening file: {error.message}")
 
 
 class MyApp(Adw.Application):
